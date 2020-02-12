9ee1e8af8ca7d0b197017d6988c94f95cc1236ae
Felix Fennell <felnne@bas.ac.uk>
2020-02-12 16:07:40 +0000

    Updating wrapper script
    Now that we can refer to the stable image (released for the first time in 0.2.2).

--- support/container-wrapper/podman-wrapper.sh
+++ support/container-wrapper/podman-wrapper.sh
@@ -7,4 +7,4 @@ normal=$(tput sgr0)
 
 printf "%40s\n\n" "${blue}Note: This command was run inside a container.${normal}"
 
-podman run --rm=true --tty --interactive --user=root --volume /users/geoweb/.config/web-map-inventory/:/home/geoweb/.config/web-map-inventory/:rw docker-registry.data.bas.ac.uk/magic/web-map-inventory/deploy:latest "$@"
+podman run --rm=true --tty --interactive --user=root --volume /users/geoweb/.config/web-map-inventory/:/home/geoweb/.config/web-map-inventory/:rw docker-registry.data.bas.ac.uk/magic/web-map-inventory/deploy:stable "$@"
